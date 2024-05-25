9d7790dc28a0c0914f6dc9fb6c36c6fdb1204788
LeTran <lethanhbaotran.letran@gmail.com>
2024-05-25 22:26:31 +0700

    sched

--- src/sched.c
+++ src/sched.c
@@ -42,22 +42,40 @@ void init_scheduler(void) {
  *  We implement stateful here using transition technique
  *  State representation   prio = 0 .. MAX_PRIO, curr_slot = 0..(MAX_PRIO - prio)
  */
-struct pcb_t * get_mlq_proc(void) {
-	struct pcb_t * proc = NULL;
-	/*TODO: get a process from PRIORITY [ready_queue].
-	 * Remember to use lock to protect the queue.
-	 */
-	int prio;
-    	for (prio = 0; prio < MAX_PRIO; prio++) {
-        	pthread_mutex_lock(&queue_lock);
-        	if (!empty(&mlq_ready_queue[prio])) {
-            		proc = dequeue(&mlq_ready_queue[prio]);
-            		pthread_mutex_unlock(&queue_lock);
-            		return proc;
-        	}
-        	pthread_mutex_unlock(&queue_lock);
-    	}
-	return NULL;	
+struct pcb_t *get_mlq_proc(void)
+{
+	struct pcb_t *proc = NULL;
+	static int prio = 0;
+	static int num_slot = MAX_PRIO;
+	pthread_mutex_lock(&queue_lock);
+	for (int i=0; i<MAX_PRIO; i++) {
+		int set = 0;
+		if (!empty(&mlq_ready_queue[prio])) {
+			proc = dequeue(&mlq_ready_queue[prio]);
+			num_slot--;
+			if (num_slot <= 0) {
+				prio++;
+				if (prio >= MAX_PRIO) prio = 0;
+				num_slot = MAX_PRIO - prio; 
+			}
+			break;
+		}
+		else {
+			for (int j=0; j<prio; j++) {
+				if (!empty(&mlq_ready_queue[j])) {
+					prio=j;
+					set=1;
+				}		
+			}
+		}
+		if ( set==0 ) {
+			prio++;
+			if (prio >= MAX_PRIO) prio = 0;
+			num_slot = MAX_PRIO - prio;
+		}
+	}
+	pthread_mutex_unlock(&queue_lock);
+	return proc;
 }
 
 void put_mlq_proc(struct pcb_t * proc) {
